3a43f3e5edfa4e6689614603aedb99a4bd7f47ce
sonjinheum <son.jinheum@gmail.com>
2026-09-17 21:55:13 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// HUMAN ERROR CHALLENGE 2
+// Firebase Configuration
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDwcQKIREN7plOZ8nqxeB7teBoDfUNtBYM",
+  authDomain: "human-error-challenge-2.firebaseapp.com",
+  databaseURL: "https://human-error-challenge-2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "human-error-challenge-2",
+  storageBucket: "human-error-challenge-2.firebasestorage.app",
+  messagingSenderId: "186398774588",
+  appId: "1:186398774588:web:d00f852d0ff5a0c5d63a39"
+};
